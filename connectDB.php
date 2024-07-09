cb597dbbe121ee0e03972f18c7071a6bffe220a2
Izwan Danial <93575615+shazwand989@users.noreply.github.com>
2024-07-09 22:20:17 +0800

    okey

--- connectDB.php
+++ connectDB.php
@@ -1,13 +1,15 @@
 <?php
 /* Database connection settings */
-	$servername = "localhost";
-    $username = "root";		//put your phpmyadmin username.(default is "root")
-    $password = "";			//if your phpmyadmin has a password put it here.(default is "root")
-    $dbname = "rfidattendance";
-    
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
-	
-	if ($conn->connect_error) {
-        die("Database Connection failed: " . $conn->connect_error);
-    }
-?>
+$servername = "localhost";
+$username = "root";        //put your phpmyadmin username.(default is "root")
+$password = "";            //if your phpmyadmin has a password put it here.(default is "root")
+$dbname = "rfidattendance";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Database Connection failed: " . $conn->connect_error);
+}
+
+$sql_alter = "ALTER TABLE `users_logs` ADD COLUMN `status` INT(1) NOT NULL DEFAULT '0'";
+$conn->query($sql_alter);
---
+++ user-log-request.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'connectDB.php';
+header('Content-Type: application/json');
+$sql_user_log = "SELECT * FROM `users_logs` WHERE `status`= '0'";
+$result = $conn->query($sql_user_log);
+$data = array(
+    'total' => $result->num_rows
+);
+echo json_encode($data);
+
+// update status to 1
+$sql_update = "UPDATE `users_logs` SET `status` = '1' WHERE `status` = '0'";
+$conn->query($sql_update);
